a3d7c22af243dbae6424fca0e88c12e4d90a0ac0
Owen Harris <owenharris21@icloud.com>
2018-06-23 16:35:58 -0600

    complete Objective1Lab3

---
+++ Labs/Objective1Lab3.java
@@ -0,0 +1,7 @@
+public class Objective1Lab3 {
+  public static void main(String[] args) {
+    System.out.println("Name: Owen Harris");
+    System.out.println("Age: 23");
+    System.out.println("State: Colorado"); 
+  }
+}
