414c142c41d04ebe0d735b6301f75cef89ce6df2
Owen Harris <owenharris21@icloud.com>
2018-06-24 10:43:41 -0600

    completed Objective2Lab4

---
+++ Labs/Objective2Lab4.java
@@ -0,0 +1,9 @@
+public class Objective2Lab4 {
+  public static void main(String[] args) {
+    double num1 = 3.0, num2 = 19.0, num3 = 21.0, average = 0.0;
+
+    average = (num1 + num2 + num3) / 3;
+
+    System.out.println(average);
+  }
+}
