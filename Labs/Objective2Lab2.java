f1ecec4251483a10876dafa38bb7c5c1a35c0e05
Owen Harris <owenharris21@icloud.com>
2018-06-24 10:15:44 -0600

    complete Objective2Lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,9 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Owen Harris";
+    int age = 23;
+    String state = "Colorado";
+
+    System.out.print("Name: " + name + " Age: " + age + " State: " + state);
+  }
+}
