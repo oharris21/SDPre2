197e03c65886125f99a21a08e6b7c9e6c6e43b8c
Owen Harris <owenharris21@icloud.com>
2018-06-24 10:35:50 -0600

    complete Objective2Lab3

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,11 @@
+public class Objective2Lab3 {
+  public static void main(String[] args) {
+    String food1 = "Grilled Chicken"; String food2 = "Hamburgers";
+    String food3 = "Sweet Potato Fries";
+
+    System.out.println("My favorite foods are: ");
+    System.out.println("1. " + food1);
+    System.out.println("2. " + food2);
+    System.out.println("3. " + food3);
+  }
+}
