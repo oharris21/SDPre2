b14d0e197c88e752ec6b5ef758dc99debac3ac8f
Owen Harris <owenharris21@icloud.com>
2018-06-26 14:37:50 -0600

    complete Objective6Lab4

---
+++ Labs/Objective6Lab4.java
@@ -0,0 +1,13 @@
+public class Objective6Lab4 {
+  public static void main(String[] args) {
+    int counter = 0;
+    int sum = 0;
+
+    while(counter < 20) {
+      counter ++;
+      sum += counter;
+    }
+    
+      System.out.println(sum);
+  }
+}
