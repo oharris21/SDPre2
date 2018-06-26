e159df3c88df464269f40fcb34aac3f5611cdb61
Owen Harris <owenharris21@icloud.com>
2018-06-26 14:12:41 -0600

    complete Objective6Lab1

---
+++ Labs/Objective6Lab1.java
@@ -0,0 +1,10 @@
+public class Objective6Lab1 {
+  public static void main(String[] args) {
+    int counter = 11;
+
+    while (counter > 1) {
+      counter = counter - 1;
+      System.out.println(counter);
+    }
+  }
+}
