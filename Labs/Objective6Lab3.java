00bf35f20c4e1382b75e7d06612ca5461922eded
Owen Harris <owenharris21@icloud.com>
2018-06-26 14:33:30 -0600

    complete Objective6Lab3

---
+++ Labs/Objective6Lab3.java
@@ -0,0 +1,15 @@
+public class Objective6Lab3 {
+  public static void main(String[] args) {
+    int counter = 0;
+
+    while(counter < 20) {
+      counter++;
+        if (counter % 2 == 0) {
+          System.out.println(counter + " is even");
+        }
+        else {
+          System.out.println(counter + " is odd");
+        }
+    }
+  }
+}
